e8f3a72fa08536a6a10de463ab445788d00655e9
RodrigoB <r.brevis01@ufromail.cl>
2023-11-29 02:06:36 -0300

    Se crea prueba para verificar lista de clubes

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -8,19 +8,38 @@ describe("Home", () => {
 	});
 
 	it("[Successful H-2] Home correct", () => {
-		// Llama al comando de login para obtener el token y establecer las credenciales
+		
 		cy.login().then(({ token, user }) => {
-			// Verifica que el token se haya obtenido correctamente
+			
 			expect(token).to.exist;
 			expect(user).to.exist;
 			expect(user.name).to.exist;
 	  
-			// Visita la página de inicio de la aplicación
+			
 			cy.visit("/", {
 			  failOnStatusCode: false,
 			});
 	  
 			cy.get('h2').should('contain', 'Welcome JORGE,')
+		  });
+	})
+
+	it("[Successful H-3] Get list clubs", () => {
+		
+		cy.login().then(({ token, user }) => {
+			
+			expect(token).to.exist;
+			expect(user).to.exist;
+			expect(user.name).to.exist;
+	  
+			
+			cy.visit("/", {
+			  failOnStatusCode: false,
+			});
+	  
+			cy.get("#6566462b15980d613a6cdc9d").should("exist");
+
+			cy.get('div').should('contain','Lakers');
 		  });
 	})
 });
