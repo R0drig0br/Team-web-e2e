2c8dff025a39bfd34bd2e7ce5a1e22238d1fb7f7
RodrigoB <r.brevis01@ufromail.cl>
2023-11-29 01:07:23 -0300

    Se crea prueba correcta para inicio de sesión

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -9,4 +9,17 @@ describe("Login", () => {
 
 		cy.get(".text-negative").should("text", "invalid credentials");
 	});
+
+	it("[Successful L-1] valid credentials", () => {
+		cy.visit("/login", {
+			failOnStatusCode: false,
+		});
+		cy.get('input[id="login-email"').type("j.cortez01@ufromail.cl");
+		cy.get('input[id="login-password"').type("e4VgaSDE");
+		cy.get("button").click();
+
+		cy.on("url:changed", () => {
+			cy.url().should("eq", `${Cypress.config().baseUrl}/`);
+		});
+	});
 });
