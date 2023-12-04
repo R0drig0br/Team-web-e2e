17547e5b678a1a42b5f8c781eb587d2ef459d6c3
Jorge Cortez <j.cortez01@ufromail.cl>
2023-12-04 02:02:02 -0300

    se agrega prueba C4 de club

--- cypress/e2e/club.cy.js
+++ cypress/e2e/club.cy.js
@@ -37,4 +37,24 @@ describe("Club", () => {
 			});
 		});
 	});
+
+	it("[Error C-4] add member without email", () => {
+		cy.login().then(({ token, user }) => {
+			cy.getClubs(token).then((clubes) => {
+				cy.visit("/", {
+					failOnStatusCode: false,
+				});
+				var originalNumber = 0;
+				cy.get(`div[id=${clubes[0]._id}]`).click();
+				cy.get('span[class="text-h3"]').contains(clubes[0].name);
+				cy.contains('New member').click();
+				cy.get('[name="member-name"]').type('Nombre ejemplo');
+				cy.get('[name="member-lastname"]').type('Apellido ejemplo');
+				cy.get('[name="member-dni"]').type('DNI ejemplo');
+				cy.get('[name="member-nickname"]').type('Nickname ejemplo');
+				cy.contains('span[class="block"]', 'Add Member').click();
+				cy.get('p').should('contain', 'email is required and must be a valid email');
+			});
+		});
+	});
 });
